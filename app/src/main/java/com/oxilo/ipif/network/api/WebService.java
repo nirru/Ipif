b8884cfe7fd53ae338095d20116a900a7b07dbee
nirru <nikk3008@outlook.com>
2018-01-09 23:21:27 +0530

    initial commit

--- app/src/main/java/com/oxilo/ipif/network/api/WebService.java
+++ app/src/main/java/com/oxilo/ipif/network/api/WebService.java
@@ -23,45 +23,45 @@ import retrofit2.http.Query;
  */
 public interface WebService {
 
-    @GET("ipif_block/index.php/api/get_categories_list")
+    @GET("ipif/index.php/api/get_categories_list")
     io.reactivex.Observable<Response<ResponseBody>> getCategory();
 
     @FormUrlEncoded
-    @POST("ipif_block/index.php/api/login")
+    @POST("ipif/index.php/api/login")
     io.reactivex.Observable<Response<ResponseBody>> login(
             @Field("phone_num") String phone,
             @Field("device_token") String device_token);
 
-    @GET("ipif_block/index.php/api/get_brands_list")
+    @GET("ipif/index.php/api/get_brands_list")
     io.reactivex.Observable<Response<ResponseBody>> getAllProduct(
             @Query("tag") String popular);
 
-    @GET("ipif_block/index.php/api/get_brands_list")
+    @GET("ipif/index.php/api/get_brands_list")
     io.reactivex.Observable<Response<ResponseBody>> getBrand(
             @Query("tag") String brand);
 
-    @GET("ipif_block/index.php/api/get_brands_list")
+    @GET("ipif/index.php/api/get_brands_list")
     io.reactivex.Observable<Response<ResponseBody>> getBrandsByLocations(
             @Query("tag") String location,
             @Query("latitude") String latitude,
             @Query("longitude") String longitude,
             @Query("miles") String miles);
 
-    @GET("ipif_block/index.php/api/get_categories_by_brand_id")
+    @GET("ipif/index.php/api/get_categories_by_brand_id")
     io.reactivex.Observable<Response<ResponseBody>> getCategoriesByBrandId(
             @Query("brand_id") String brand_id);
 
-    @GET("ipif_block/index.php/api/get_products_list")
+    @GET("ipif/index.php/api/get_products_list")
     io.reactivex.Observable<Response<ResponseBody>> get_products_list(
             @Query("category_id") String category_id);
 
-    @GET("ipif_block/index.php/api/get_products_detail")
+    @GET("ipif/index.php/api/get_products_detail")
     io.reactivex.Observable<Response<ResponseBody>> get_products_detail(
             @Query("product_id") String product_id);
 
     @Headers("Accept: multipart/form-data")
     @Multipart
-    @POST("ipif_block/index.php/api/userprofile")
+    @POST("ipif/index.php/api/userprofile")
     io.reactivex.Observable<Response<ResponseBody>> uploadProfile(@Part MultipartBody.Part image,
                                                      @Part("name") RequestBody name,
                                                      @Part("user_id") RequestBody user_id);
